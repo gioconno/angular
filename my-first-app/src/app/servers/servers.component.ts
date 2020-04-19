7da9345f0a037659438bf9cc427de1a405a9325d
Riccardo Zanchi <riccardo.zanchi@csttech.it>
2020-04-19 15:20:26 +0200

    Lesson 4

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+
+  // selector: 'app-servers' // tag
+  // selector: '[app-servers]', // attributo
+  selector: '.app-servers', // classe
+  template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
