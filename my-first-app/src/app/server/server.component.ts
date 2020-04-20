d5c83e0bbc7aa7cfa9e0e7c8148c67e45046adbd
Riccardo Zanchi <riccardo.zanchi@csttech.it>
2020-04-20 15:27:20 +0200

    Lesson 10

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -8,10 +8,19 @@ import { Component } from '@angular/core';
 
 export class ServerComponent {
     serverId = 10;
-    serverName = 'Server-10'
-    serverStatus = 'OFFLINE';
+    serverName = 'Server-10';
+    serverStatus = '';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerName() {
         return this.serverName;
     }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
+
 }
