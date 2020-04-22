377b33d3ff2c7c8106fc02c2cf704451f212b20a
Riccardo Zanchi <riccardo.zanchi@csttech.it>
2020-04-22 11:07:02 +0200

    Course project lesson 4

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2018/11/05/12/42/food-3796017_960_720.jpg'), 
+    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2018/11/05/12/42/food-3796017_960_720.jpg')
+  ];
 
   constructor() { }
 
