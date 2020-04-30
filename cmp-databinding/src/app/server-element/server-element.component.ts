e24a520a2c5bbaa090ff8865bc50d68df7749b35
Riccardo Zanchi <riccardo.zanchi@csttech.it>
2020-04-30 09:30:37 +0200

    Components databinding lesson 8

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('serverElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('serverElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constuctor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
 
 }
