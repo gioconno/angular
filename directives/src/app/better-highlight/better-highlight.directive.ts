7fb41bf195149fdca271c959f3c2edf1c14cc298
rik <p3mal88@gmail.com>
2020-05-08 14:56:27 +0200

    Directive with hostbinding

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,26 +1,33 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.color') textColor = 'black';
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  }
   ngOnInit(): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
   }
 
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+
+    this.backgroundColor = 'blue';
+    this.textColor = 'white';
   }
 
   @HostListener('mouseleave') mouseLeave(eventDate: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black ');
+
+    this.backgroundColor = 'transparent';
+    this.textColor = 'black';
   }
 
 }
