c846ea2a732a87fa2f413dbf1570f1f4b628f1e8
rik <p3mal88@gmail.com>
2020-05-08 14:35:27 +0200

    Directive with render

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHiglightDirective } from './basic-higlight/basic-higlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHiglightDirective
+    BasicHiglightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
