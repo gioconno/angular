b7de3355608a631f9412a39f07014c3bdfd008cd
rik <p3mal88@gmail.com>
2020-05-08 14:18:21 +0200

    Basic directive

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHiglightDirective } from './basic-higlight/basic-higlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHiglightDirective
   ],
   imports: [
     BrowserModule,
