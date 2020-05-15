274d9c4f3eaec8378ef7ac233d27a6abbf408f89
rik <p3mal88@gmail.com>
2020-05-15 11:43:11 +0200

    Added event to clsoe the dropdown menu in the entire document

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
@@ -7,8 +7,16 @@ export class DropdownDirective {
 
     @HostBinding('class.open') isOpen = false;
 
-    @HostListener('click') toggleOpen(eventData: Event) {
-        this.isOpen = !this.isOpen;
+    // This code will register the click only in the defined element (e.g. a dropdown button).
+    // If you click outside the element, this will not close the dropdown: only clicking the element itself will close it.
+    // @HostListener('click') toggleOpen(eventData: Event) {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    // This code instead opens the dropwdown element and can close it if you click anywhere in the document
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+    constructor(private elRef: ElementRef) { }
 
 }
