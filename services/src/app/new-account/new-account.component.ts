0a7393a1706f1061d84c93952fa215e65b1c1b6d
rik <p3mal88@gmail.com>
2020-05-19 11:09:50 +0200

    Added services for accounts and injected them

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,22 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  constructor(private logService: LoggingService, private accountService: AccountService) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
-    // This works, but it is a wrong way to implement services in Angular
-    // const logService = new LoggingService();
-    // logService.logStatusChange(accountStatus);
+    this.accountService.addAccont(accountName, accountStatus); 
+    this.logService.logStatusChange(accountStatus);
   }
 }
