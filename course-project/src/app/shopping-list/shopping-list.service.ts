8dfd626151c4b30286723ed1d9e76b9fa544718e
rik <p3mal88@gmail.com>
2020-05-19 15:10:41 +0200

    Added handling of new ingredient

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,14 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ShoppingListService {
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
     ];
+
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
 
     ingredientAdded = new EventEmitter<Ingredient>();
 
@@ -16,6 +18,7 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
 
