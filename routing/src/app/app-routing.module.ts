32b234b4e1a18c1735c36b98fcc3d29bd2167b41
rik <p3mal88@gmail.com>
2020-05-21 15:42:03 +0200

    CanDeactivate guard

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { AuthGuard } from './auth-guard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -22,7 +23,7 @@ const appRoutes: Routes = [
         canActivateChild: [AuthGuard],
         component: ServersComponent, children: [
             { path: ':id', component: ServerComponent },
-            { path: ':id/edit', component: EditServerComponent }
+            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
         ]
     },
     { path: '', component: HomeComponent },
