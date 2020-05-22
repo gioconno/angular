a7a3725763eca1f5fd7968c5bec44766a2fb36eb
rik <p3mal88@gmail.com>
2020-05-22 10:44:35 +0200

    Added routing to the project

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from "./shared/dropdown.directive";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
    declarations: [
@@ -26,7 +27,8 @@ import { DropdownDirective } from "./shared/dropdown.directive";
    ],
    imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      AppRoutingModule
    ],
    providers: [],
    bootstrap: [
