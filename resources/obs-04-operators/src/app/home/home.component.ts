f8b2b7655496f2896f2609e1c73c8184bf62267b
rik <p3mal88@gmail.com>
2020-05-29 09:49:04 +0200

    operators

--- resources/obs-04-operators/src/app/home/home.component.ts
+++ resources/obs-04-operators/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
@@ -10,31 +10,31 @@ import { map, filter } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  private countSubscription: Subscription;
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    // this.countSubscription = interval(1000).subscribe(count => {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
-
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
-        if (count === 2) {
+        observer.next(count);
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
-          observer.error(new Error('Count is greater than 3!'));
+          observer.error(new Error('Count is greater 3!'));
         }
-        observer.next(count);
         count++;
       }, 1000);
     });
 
 
-    this.countSubscription = customIntervalObservable.pipe(filter(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
       return data > 0;
     }), map((data: number) => {
       return 'Round: ' + (data + 1);
@@ -42,15 +42,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       console.log(data);
     }, error => {
       console.log(error);
-      alert(error);
+      alert(error.message);
     }, () => {
-      console.log('completed');
+      console.log('Completed!');
     });
-
   }
 
   ngOnDestroy(): void {
-    this.countSubscription.unsubscribe();
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
